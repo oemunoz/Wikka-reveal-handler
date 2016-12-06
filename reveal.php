4996df0de2f6254e77934904170517780cca7072
Oscar Munoz <oscaremu@gmail.com>
2016-12-06 16:49:09 -0500

    Now the paths to css/js are fixed if you use another path.

--- reveal.php
+++ reveal.php
@@ -1,5 +1,29 @@
 <?php
 $site_base = WIKKA_BASE_URL;
+// Make robust the way to find the css and the js. The handler can be on many directorys.
+$relative_path = explode( ',', $this->GetConfigValue('handler_path'));
+$my_handler_path = "plugins/handlersx";
+// Check for the css and the js files. If exist overwrite the black/withe web.
+foreach ($relative_path as $key => $value) {
+	if ( is_file($relative_path[$key].'/reveal/css/reveal.css') &&
+	     is_file($relative_path[$key].'/reveal/css/theme/black.css') &&
+	     is_file($relative_path[$key].'/reveal/lib/css/zenburn.css') &&
+	     is_file($relative_path[$key].'/reveal/css/print/pdf.css') &&
+	     is_file($relative_path[$key].'/reveal/css/print/paper.css') &&
+	     is_file($relative_path[$key].'/reveal/lib/js/head.min.js') &&
+	     is_file($relative_path[$key].'/reveal/js/reveal.js') &&
+	     is_file($relative_path[$key].'/reveal/plugin/markdown/marked.js') &&
+	     is_file($relative_path[$key].'/reveal/plugin/notes/notes.js') &&
+	     is_file($relative_path[$key].'/reveal/plugin/highlight/highlight.js')
+		 ) {
+			 		$my_handler_path = $relative_path[$key];
+			 }
+	}
+/*
+
+}
+       }}
+*/
 ?>
 <!doctype html>
 <html>
@@ -9,18 +33,18 @@ $site_base = WIKKA_BASE_URL;
 
 		<title>reveal.js</title>
 
-		<link rel="stylesheet" href="<?php echo $site_base ?>plugins/handlers/reveal/css/reveal.css">
-		<link rel="stylesheet" href="<?php echo $site_base ?>plugins/handlers/reveal/css/theme/black.css">
+		<link rel="stylesheet" href="<?php echo $my_handler_path ?>/reveal/css/reveal.css">
+		<link rel="stylesheet" href="<?php echo $my_handler_path ?>/reveal/css/theme/black.css">
 
 		<!-- Theme used for syntax highlighting of code -->
-		<link rel="stylesheet" href="<?php echo $site_base ?>plugins/handlers/reveal/lib/css/zenburn.css">
+		<link rel="stylesheet" href="<?php echo $site_base.$my_handler_path ?>/reveal/lib/css/zenburn.css">
 
 		<!-- Printing and PDF exports -->
 		<script>
 			var link = document.createElement( 'link' );
 			link.rel = 'stylesheet';
 			link.type = 'text/css';
-			link.href = window.location.search.match( /print-pdf/gi ) ? '<?php echo $site_base ?>plugins/handlers/reveal/css/print/pdf.css' : '<?php echo $site_base ?>plugins/handlers/reveal/css/print/paper.css';
+			link.href = window.location.search.match( /print-pdf/gi ) ? '<?php echo $my_handler_path ?>/reveal/css/print/pdf.css' : '<?php echo $my_handler_path ?>/reveal/css/print/paper.css';
 			document.getElementsByTagName( 'head' )[0].appendChild( link );
 		</script>
 	</head>
@@ -38,8 +62,8 @@ $site_base = WIKKA_BASE_URL;
 			</div>
 		</div>
 
-		<script src="<?php echo $site_base ?>plugins/handlers/reveal/lib/js/head.min.js"></script>
-		<script src="<?php echo $site_base ?>plugins/handlers/reveal/js/reveal.js"></script>
+		<script src="<?php echo $my_handler_path ?>/reveal/lib/js/head.min.js"></script>
+		<script src="<?php echo $my_handler_path ?>/reveal/js/reveal.js"></script>
 
 		<script>
 			// More info https://github.com/hakimel/reveal.js#configuration
@@ -48,10 +72,10 @@ $site_base = WIKKA_BASE_URL;
 
 				// More info https://github.com/hakimel/reveal.js#dependencies
 				dependencies: [
-					{ src: '<?php echo $site_base ?>plugins/handlers/reveal/plugin/markdown/marked.js' },
-					{ src: '<?php echo $site_base ?>plugins/handlers/reveal/plugin/markdown/markdown.js' },
-					{ src: '<?php echo $site_base ?>plugins/handlers/reveal/plugin/notes/notes.js', async: true },
-					{ src: '<?php echo $site_base ?>plugins/handlers/reveal/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
+					{ src: '<?php echo $my_handler_path ?>/reveal/plugin/markdown/marked.js' },
+					{ src: '<?php echo $my_handler_path ?>/reveal/plugin/markdown/markdown.js' },
+					{ src: '<?php echo $my_handler_path ?>/reveal/plugin/notes/notes.js', async: true },
+					{ src: '<?php echo $my_handler_path ?>/reveal/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
 				]
 			});
 		</script>
